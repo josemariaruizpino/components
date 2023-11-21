91d6d7e8f890f6de46bcb45ae5f9224d3a9b4137
Jose Maria Ruiz Pino <jose.ruiz@icp.es>
2023-11-21 08:56:34 +0100

    first commit

---
+++ components/ActionsPage.razor.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace Perseo.Scl.BackOffice.UI.Shared
+{
+    public partial class ActionsPage
+    {
+        [Parameter]
+        public List<ActionGroup> ActionGroups { get; set; }
+    }
+
+    public class ActionGroup
+    {
+        public string Title { get; set; }
+        public List<ActionItem> ActionItems { get; set; }
+
+    }
+    public class ActionItem
+    {
+        public string ItemName { get; set; }
+        /// <summary>
+        /// Marca si el botón está activo o no
+        /// </summary>
+        public bool ItemActive { get; set; }
+        public string ItemIcon { get; set; }
+        public bool ItemDissabled { get; set; }
+        public EventCallback<ActionItem> OnClick { get; set; }
+       public int ID { get; set; } = int.MinValue;
+        public bool SubAction { get; set; }
+
+       
+
+
+        public List<SubActionGroup> SubActionGroup { get; set; }
+    }
+
+    public class SubActionGroup {
+        public string SubActionGroupTitle { get; set; }
+        public List<SubActionItem> SubActionItems { get; set; }
+    }
+
+    public class SubActionItem {
+        public string SubItemName { get; set; }
+        public string SubItemIcon { get; set; }
+        public bool SubItemDissabled { get; set; }
+
+        public EventCallback<SubActionItem> OnClick { get; set; }
+    }
+}
